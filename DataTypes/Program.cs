66a0095051e08b83f86aea7b2f51acc14b36c20f
francis mcdermott <francismcd97@gmail.com>
2022-06-08 12:23:39 +0100

    double and floats

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -15,13 +15,26 @@ namespace DataTypes
 
             //declare and itialising a variable
             int num2 = 23;
-            
+            long myLongNum = 123131231312312313;
             num2 = 100;
             //using concatination
             Console.WriteLine("num1 is " + num1);
             int sum = num1 + num2;
             Console.WriteLine("num1 " + num1 + " +" + " num2 " + num2 + " is " + sum);
-            
+
+            double d1 = 3.1415;
+            double d2 = 5.1;
+            double dDiv = d1 / d2;
+            Console.WriteLine("d1/d2 is " + dDiv);
+
+            float f1 = 3.1415f;
+            float f2 = 5.1f;
+            float fDiv = f1/ f2;
+            Console.WriteLine("f1/f2 is " + fDiv);
+
+            //divide double d1 by int num1
+            double diDiv = d1 / num1;
+            Console.WriteLine("d1/num1 is " + diDiv);
             
 
             Console.Read();
